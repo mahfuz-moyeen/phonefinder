7959abe29f6af08a737c9f8cf852349447cea90e
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-03-01 01:52:55 +0600

    get phone details

--- js/app.js
+++ js/app.js
@@ -16,16 +16,23 @@ const displayResult = results => {
         const div = document.createElement('div');
         div.className = 'col';
         div.innerHTML = `
-            <div class="card">
-                <img src="${result.image}" class="card-img-top" alt="...">
-                <div class="card-body">
+            <div class="card border-0">
+                <img src="${result.image}" class="card-img-top w-50 mx-auto mt-3" alt="${result.phone_name}">
+                <div class="card-body text-center">
                     <h5 class="card-title">${result.phone_name}</h5>
-                    <p class="card-text">Band: ${result.brand}</p>
+                    <p class="card-text mb-2">Band: ${result.brand}</p>
+                    <button onclick="getDetails('${result.slug}')" class="btn btn-info">See Details</button>
                 </div>
             </div>
          `;   
-        searchResult.appendChild(div);   
+        searchResult.appendChild(div); 
     });
+};
+
+
+const getDetails = id => {
+    console.log(id);
+
 };
 
 
