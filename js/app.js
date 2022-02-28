d1975bb806a1b01c2d4b5c5e7875a664ac7ae62f
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-03-01 01:36:52 +0600

    get phone to api

--- js/app.js
+++ js/app.js
@@ -1,11 +1,38 @@
 
+// html section id 
+const searchResult = document.getElementById('search-result');
 
 // get data by search
-const getSearch = () =>{
+const getSearch = () => {
 
     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-    .then(response => response.json())
-    .then(data => console.log(data))
+        .then(response => response.json())
+        .then(data => displayResult(data.data))
 };
 
-getSearch();
+const displayResult = results => {
+    results.forEach(result => {
+        console.log(result)
+        const div = document.createElement('div');
+        div.className = 'col';
+        div.innerHTML = `
+            <div class="card">
+                <img src="${result.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${result.phone_name}</h5>
+                    <p class="card-text">Band: ${result.brand}</p>
+                </div>
+            </div>
+         `;   
+        searchResult.appendChild(div);   
+    });
+};
+
+
+/*
+{brand: 'Apple ', phone_name: 'iPhone 13 mini', slug: 'apple_iphone_13_mini-11104', image: 'https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-mini.jpg'}
+brand: "Apple "
+image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-mini.jpg"
+phone_name: "iPhone 13 mini"
+slug: "apple_iphone_13_mini-11104"
+*/
